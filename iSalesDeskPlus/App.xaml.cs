ca50a0a95f5a128edd9ef59113c83b04e73b459d
Fernando Beltrán <ferbeltran@icloud.com>
2019-07-10 18:29:07 -0600

    Add project files.

---
+++ iSalesDeskPlus/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Prism;
+using Prism.Ioc;
+using iSalesDeskPlus.ViewModels;
+using iSalesDeskPlus.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace iSalesDeskPlus
+{
+    public partial class App
+    {
+ 
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/Login");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<Login, LoginViewModel>();
+        }
+    }
+}
---
+++ iSalesDeskPlus/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace iSalesDeskPlus.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        public LoginViewModel(INavigationService navigationService) : base(navigationService)
+        {
+
+        }
+    }
+}
